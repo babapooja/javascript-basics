975836b91269b4a19eef08a556f0a1ed71d9adaa
pb17773 <pooja.baba@zs.com>
2021-02-22 17:12:38 +0530

    added different ways of using map function

--- src/map.js
+++ src/map.js
@@ -27,18 +27,22 @@ const people = [
     }
 ];
 
-const ages = people.map((person)=>person.age+10);
-console.log(ages);
 
-const newPeople = people.map((person)=>{
+// iterate over the array and fetch only one value from the object of each array
+const ages = people.map((person) => person.age);
+document.querySelector('#ages').innerHTML = `<strong>Ages are:</strong> ${ages.join(', ')}`;
+
+// modify existing JSON keys by using an external function
+const modifyJOSNkeys = (person) => {
     return {
         firstName: person.name.toUpperCase(),
-        oldAge: person.age+10,
+        oldAge: person.age + 10,
     }
-})
+}
+const newPeople = people.map(modifyJOSNkeys)
 
-console.log(newPeople);
 
-const names = people.map((item)=>`<span>${item.name}</span>`);
-const result = document.querySelector('#result');
+// fetch the names and display the names in a div having id names
+const names = people.map((item) => `<span>${item.name}</span>`);
+const result = document.querySelector('#names');
 result.innerHTML = `<strong>Names are:</strong> ${names.join(', ')}`;
